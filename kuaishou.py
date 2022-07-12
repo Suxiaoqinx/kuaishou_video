357a863cdae628a9c78be1f0e8c9ad5ba5aa96cb
苏晓晴 <37541680+Suxiaoqinx@users.noreply.github.com>
2022-07-12 23:23:05 +0800

    Update kuaishou.py
    优化快手解析

--- kuaishou.py
+++ kuaishou.py
@@ -1,16 +1,13 @@
 import requests
 import json
 import re 
-
-geturl = "https://v.kuaishouapp.com/s/NTuYIMTy" #替换为你的视频链接
-# 请求网页
-response = requests.get(geturl)
+response = requests.get(input('请输入你需要解析的快手链接:'))
 #正则获取链接中的快手视频id
-surl = re.findall('photoId=(.*?)&',response.url)
+ksid = re.findall('photoId=(.*?)&',response.url)
 url = "https://v.m.chenzhongtech.com/rest/wd/photo/info"
-payload = '{"photoId": "'+surl[0]+'","isLongVideo": false}'
+payload = '{"photoId": "'+ksid[0]+'","isLongVideo": false}'
 headers = {
-   'Cookie': 'did=web_9bceee20fa5d4a968535a27e538bf51b; didv=1655992503000;', #如果失效了的话 请更新一下cookie！
+   'Cookie': 'did=web_9bceee20fa5d4a968535a27e538bf51b; didv=1655992503000;', #如果解析失败可以尝试替换cookie
    'Referer': response.url,
    'Content-Type': 'application/json'
 }
@@ -18,7 +15,7 @@ headers = {
 response = requests.request("POST", url, headers=headers, data=payload)
 #格式化json
 jsonobj = json.loads(response.text)
-print(
-   '视频标题：'+jsonobj['photo']['caption']+'\n'+
-   '视频图片：'+jsonobj['photo']['coverUrls'][0]['url']+'\n'+
-   '视频直链：'+jsonobj['photo']['mainMvUrls'][0]['url'])
+if jsonobj['photo']['mainMvUrls'][0]['url'] in jsonobj:
+    print('获取视频直链失败！')
+else:
+    print('提示：获取成功！' + '\n'+'视频标题：'+jsonobj['photo']['caption']+'\n'+'视频图片：'+jsonobj['photo']['coverUrls'][0]['url']+'\n'+'视频直链：'+jsonobj['photo']['mainMvUrls'][0]['url']+'\n'+'作者：'+jsonobj['photo']['userName'])
